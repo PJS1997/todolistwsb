c2399cf032946173c1652fb4169d7390e2d62d92
DESKTOP-KCGLTP0\Dom <pawel.jan.stanislaw@gmail.com>
2021-03-30 06:37:24 +0200

    sql_commit

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
   'db' => [
-    'host' => ' remotemysql.com',
+    'host' => 'remotemysql.com',
     'database' => 'GojisI8stj',
     'user' => 'GojisI8stj',
     'password' => 'cKnXiD8Yo8'
